102ee4460e051781a085e4f2a7fd15d9a60cb64f
omprakash-singh <omprakashingh11009@gmail.com>
2021-12-28 18:04:48 +0530

    commit

--- jsfile/datecal.js
+++ jsfile/datecal.js
@@ -1,5 +1,18 @@
 const menuclose = document.querySelector(".menu");
+const from = document.getElementById("from");
+const to = document.getElementById("to");
+const cal = document.getElementById("calculate");
+const result = document.getElementById("result");
 
 let menubar = () => {
     menuclose.classList.toggle("open");
 };
+
+cal.addEventListener('click', () => {
+
+    if (from.value === null) {
+        result.innerHTML = "No value";
+    }
+    result.innerHTML = from.value;
+
+});
